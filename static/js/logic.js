75f090ed2ce3824b08e85b97038ac3fb27e6d83e
benjaminpodell <benpodellsmu@gmail.com>
2019-10-16 13:51:38 -0500

    Added Circle Magnitudes And Color Coded Legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,26 +1,26 @@
 // Storing the API endpoint's into a query url variable
-var earth_quake_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-var tectonic_plate_url = "https://raw.githubusercontent.com/fraxen/tectonic_plates/master/GeoJSON/PB2002_boundaries.json"
+var earthquake_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+var tectonicplates_url = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_steps.json"
 
 
 // Using d3 to parse the jason data in order to obtain it
-d3.json(earth_quake_url, function(data) {
-    create_features(data.features);
+d3.json(earthquake_url, function(data) {
+    createFeatures(data.features);
 });
 
 // Defining the function to run on each feature when the pop up it displayed
-function create_features(quake_data) {
-    var earthquakes = L.geoJSON(quake_data, {
+function createFeatures(earthquake_data) {
+    var earthquakes = L.geoJSON(earthquake_data, {
         onEachFeature: function(feature, layer) {
-            layer.bindPopup("<h3> Magnitude: " + feature.properties.mag +"</h3><h3> Location: "+ feature.properties.place +
+            layer.bindPopup("<h3>Magnitude: " + feature.properties.mag +"</h3><h3>Location: "+ feature.properties.place +
               "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
           },
 
           // Adding the attributes to each point when displayed
-          point_to_layer: function (feature, latlng) {
+          pointToLayer: function (feature, latlng) {
             return new L.circle(latlng,
-              {radius: radiusgrabber(feature.properties.mag),
-              fillColor: color_grabber(feature.properties.mag),
+              {radius: radiusGrabber(feature.properties.mag),
+              fillColor: colorGrabber(feature.properties.mag),
               fillOpacity: .6,
               color: "#000",
               stroke: true,
@@ -29,12 +29,13 @@ function create_features(quake_data) {
         }
         });
 
-    create_map(earthquakes);
+    createMap(earthquakes);
 }
 
 // Function that is reading in my api and accessing the lightmap, outdoors, and satellite data to render on the page
-function create_map(earthquakes) {
+function createMap(earthquakes) {
 
+    // Define map layers
     var lightMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
         maxZoom: 18,
         id: "mapbox.light",
@@ -54,71 +55,71 @@ function create_map(earthquakes) {
     });
 
 
-    // Define all layer maps that are bases
-    var layer_maps = {
+     // Define all layer maps that are bases
+     var baseMaps = {
         "Light Map": lightMap,
         "Outdoors": outdoors,
         "Satellite": satellite
     };
 
     // Create tectonic layer variable that stores the layergroup
-    var tectonic_plates = new L.LayerGroup();
+    var tectonicplates = new L.LayerGroup();
 
     // Create overlay object to hold overlay layer
-    var overlay_maps = {
+    var overlayMaps = {
         "Earthquakes": earthquakes,
-        "Tectonic Plates": tectonic_plates
+        "Tectonic Plates": tectonicplates
     };
 
     // Variable that stores the map that is layered on top of everything on page
-    var generated_map = L.map("map", {
-        center: [40.7, -94.5],
+    var map_display = L.map("map", {
+        center: [40, -95],
         zoom: 3,
-        layers: [lightMap, earthquakes, tectonic_plates]
+        layers: [lightMap, earthquakes, tectonicplates]
     });
 
     // Add tectonic plates data
-    d3.json(tectonic_plate_url, function(tonic_data) {
-        L.geoJson(tonic_data, {
+    d3.json(tectonicplates_url, function(tectonicdata) {
+        L.geoJson(tectonicdata, {
             color: "yellow",
             weight: 2
         })
-        .addTo(tectonic_plates);
+        .addTo(tectonicplates);
     });
 
     //Add layer control to map for generating differen maps on the same page
-    L.control.layers(layer_maps, overlay_maps, {
+    L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
-    }).addTo(generated_map);
+    }).addTo(map_display);
 
     // Creating a legend to display the legend
     var legend = L.control({
-        position: "bottomright"
+        position: "bottomleft"
     });
 
     // Adding the legend to the page referencing the div tab
-    legend.onAdd = function(generated_map) {
+    legend.onAdd = function(map) {
         var div = L.DomUtil.create("div", "info legend"),
-        gradients = [0, 1, 2, 3, 4, 5],
+        gradient = [0, 1, 2, 3, 4, 5],
         labels = [];
 
-    // Creating a ledgend where we reference the first one and go to the length of gradients on the ledgend and adding gradient attributes
-    for (var i = 0; i < gradients.length; i++) {
+    // Creating a legend where we reference the first one and go to the length of gradients and add gradient attributes
+    for (var i = 0; i < gradient.length; i++) {
         div.innerHTML +=
-            '<i style="background:' + color_grabber(gradients[i] + 1) + '"></i> ' +
-            gradients[i] + (gradients[i + 1] ? '&ndash;' + gradients[i + 1] + '<br>' : '+');
+            '<i style="background:' + colorGrabber(gradient[i] + 1) + '">&nbsp&nbsp&nbsp&nbsp</i> ' +
+            gradient[i] + (gradient[i + 1] ? '&ndash;' + gradient[i + 1] + '<br>' : '+');
     }
     return div;
     };
-    legend.addTo(generated_map);
+    legend.addTo(map_display);
 }
 
 // Creating the color function that give all the earthquake pop ups color based on it's magnitude
-function color_grabber(magnitude) {
+function colorGrabber(magnitude) {
     if (magnitude > 5) {
         return 'red'
     } else if (magnitude > 4) {
-        return 'orange'  
+        return 'orange'
     } else if (magnitude > 3) {
         return 'yellow'
     } else if (magnitude > 2) {
@@ -130,7 +131,7 @@ function color_grabber(magnitude) {
     }
 };
 
-//Creating the radius of the function 
-function radiusgrabber(magnitude) {
-    return magnitude * 25000;
+//Creating the radius of the function
+function radiusGrabber(magnitude) {
+    return magnitude * 30000;
 };
